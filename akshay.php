82aac6d9d8fdd7f4f6586c6f1276fc5317324d25
akshayjyothish <87229319+akshayjyothish@users.noreply.github.com>
2021-07-10 16:38:19 +0530

    Create akshay.php

---
+++ akshay.php
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @akshayjyothish
+- 👀 I’m interested in Web Designing
+- 🌱 I’m currently learning Languages like C, C++, HTML, CSS, Python, PHP, Javascript
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+akshayjyothish/akshayjyothish is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
